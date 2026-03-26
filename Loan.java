9cf36ef1147417fd7b7d37c9706630e3a596cda9
Aynica Chondamma BC <aynica2006@gmail.com>
2026-03-26 11:10:49 +0530

    Updated

---
+++ Loan.java
@@ -0,0 +1,11 @@
+public class Loan {
+    public static void main(String[] args) {
+        String name="John";
+        int age=21;
+        double salary=23000;
+        if(age>=21 && salary >=21000)
+         System.out.println(name+ " is eligible to get loan");
+         else
+         System.out.println(name+ " is not eligible get loan");
+    }
+}
